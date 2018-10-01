732b80522f0fca255c87ba3e7519255be63d6d21
fedefna <fede.fna@hotmail.com>
2018-09-30 22:04:25 -0300

    Final

---
+++ TP2/Entidades/Dulce.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2018
+{
+    public class Dulce : Producto
+    {
+        /// <summary>
+        /// Constructor que asigna los valores introducidos
+        /// </summary>
+        /// <param name="marca"></param>La marca
+        /// <param name="patente"></param>El codigo de barras del producto
+        /// <param name="color"></param>El color del empaque
+        public Dulce(EMarca marca, string patente, ConsoleColor color):base(patente,marca,color)
+        {
+        }
+
+        /// <summary>
+        /// Los dulces tienen 80 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 80;
+            }
+        }
+
+        /// <summary>
+        /// Devuelve todos los datos del Producto.
+        /// </summary>
+        /// <returns></returns>
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("DULCE");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("CALORIAS : {0}", this.CantidadCalorias);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ TP2/Entidades/Leche.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Entidades_2018
+{
+    public class Leche : Producto
+    {
+        /// <summary>
+        /// Enumerado con los distintos tipos de leche posibles.
+        /// </summary>
+        public enum ETipo { Entera, Descremada }
+        private ETipo tipo;
+
+        /// <summary>
+        /// Por defecto, TIPO será ENTERA
+        /// </summary>
+        /// <param name="marca"></param> Marca del producto
+        /// <param name="patente"></param>Codigo de barras
+        /// <param name="color"></param> Color del empaque
+        public Leche(EMarca marca, string patente, ConsoleColor color)
+            : base(patente, marca, color)
+        {
+            tipo = ETipo.Entera;
+        }
+
+        /// <summary>
+        /// Por defecto, TIPO será ENTERA. Segunto constructor
+        /// </summary>
+        /// <param name="marca"></param> Marca del producto
+        /// <param name="patente"></param>Codigo de barras
+        /// <param name="color"></param> Color del empaque
+        public Leche(EMarca marca, string patente, ConsoleColor color, ETipo tipo):this(marca,patente,color)
+        {
+        }
+
+        /// <summary>
+        /// Las leches tienen 20 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 20;
+            }
+        }
+
+        /// <summary>
+        /// Devuelve todos los datos del Producto.
+        /// </summary>
+        /// <returns></returns>
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("LECHE");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("CALORIAS : {0}", this.CantidadCalorias);
+            sb.AppendLine("TIPO : " + this.tipo);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
