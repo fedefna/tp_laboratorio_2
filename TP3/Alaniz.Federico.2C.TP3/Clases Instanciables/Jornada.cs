3ad38d47b14171d8677502c7f7ddef53367a4a8b
alumno <alumno>
2018-11-07 21:38:48 -0300

    asd

--- TP3/Alaniz.Federico.2C.TP3/Clases Instanciables/Jornada.cs
+++ TP3/Alaniz.Federico.2C.TP3/Clases Instanciables/Jornada.cs
@@ -8,5 +8,106 @@ namespace Clases_Instanciables
 {
     class Jornada
     {
+        #region Atributos
+        private List<Alumno> alumnos;
+        private Universidad.EClases clase;
+        private Profesor instructor;
+        #endregion
+
+        #region Properties
+        public Universidad.EClases Clase { get => clase; set => clase = value; }
+        public List<Alumno> Alumnos { get => alumnos; set => alumnos = value; }
+        public Profesor Instructor { get => instructor; set => instructor = value; }
+        #endregion
+
+        #region Metodos
+        /// <summary>
+        /// Constructor privado que inicializa la lista de alumnos
+        /// </summary>
+        private Jornada()
+        {
+            this.alumnos = new List<Alumno>();
+        }
+        /// <summary>
+        /// Constructor publico que setea clase, instructor y llama al privado
+        /// </summary>
+        /// <param name="clase"></param>
+        /// <param name="instructor"></param>
+        public Jornada(Universidad.EClases clase, Profesor instructor):this()
+        {
+            this.Clase = clase;
+            this.Instructor = instructor;
+        }
+        /// <summary>
+        /// Guardar de clase guardará los datos de la Jornada en un archivo de texto.
+        /// </summary>
+        /// <param name="jornada">Jornada de la cual se sacan los datos</param>
+        /// <returns>true or false</returns>
+        public bool Guardar(Jornada jornada)
+        {
+            bool retorno = false;
+
+
+
+            return retorno;
+        }
+
+        public string Leer()
+        {
+            string retorno="";
+
+            return retorno;
+        }
+
+        /// <summary>
+        /// Una Jornada será igual a un Alumno si el mismo participa de la clase
+        /// </summary>
+        /// <param name="j">jornada a comparar</param>
+        /// <param name="a">alumno a comparar</param>
+        /// <returns>True or false</returns>
+        public static bool operator ==(Jornada j, Alumno a)
+        {
+            return j.Alumnos.Contains(a);
+        }
+        /// <summary>
+        /// Una Jornada será distinta a un Alumno si el mismo no participa de la clase
+        /// </summary>
+        /// <param name="j">jornada a comparar</param>
+        /// <param name="a">alumno a comparar</param>
+        /// <returns>true or false</returns>
+        public static bool operator !=(Jornada j, Alumno a)
+        {
+            return !(j==a);
+        }
+        /// <summary>
+        /// Agregar Alumnos a la clase por medio del operador +, validando que no estén previamente cargados.
+        /// </summary>
+        /// <param name="j">jornada a agregar alumnos</param>
+        /// <param name="a">alumnoa agregar en la jornada</param>
+        /// <returns>devuelve la jornada actualizada</returns>
+        public static Jornada operator +(Jornada j, Alumno a)
+        {
+            if (!(j.Alumnos.Contains(a)))
+            {
+                j.Alumnos.Add(a);
+            }
+            return j;
+        }
+
+        /// <summary>
+        /// ToString mostrará todos los datos de la Jornada.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("Clase: {0}/nInstructor: {1}/nLista de Alumnos:",this.Clase,this.Instructor.ToString());
+            foreach(Alumno a in this.Alumnos)
+            {
+                sb.AppendFormat("/n{0}",a);
+            }
+            return sb.ToString();
+        }
+        #endregion
     }
 }
--- TP3/Alaniz.Federico.2C.TP3/Clases Instanciables/Profesor.cs
+++ TP3/Alaniz.Federico.2C.TP3/Clases Instanciables/Profesor.cs
@@ -20,14 +20,14 @@ namespace Clases_Instanciables
         /// </summary>
         public Profesor()
         {
-
+            
         }
         /// <summary>
         /// constructor privado
         /// </summary>
-        private Profesor()
+        static Profesor()
         {
-
+            random = new Random();
         }
         /// <summary>
         /// constructor
@@ -38,9 +38,86 @@ namespace Clases_Instanciables
         /// <param name="dni"></param>
         /// <param name="nacionalidad"></param>
         public Profesor(int id, string nombre,string apellido,string dni,ENacionalidad nacionalidad)
+            : base(id, nombre, apellido, dni, nacionalidad)
         {
-
+            this.clasesDelDia= new Queue<Universidad.EClases>();
+            this._randomClases();
         }
+        #endregion
+
+
+        #region Metodos
+        /// <summary>
+        /// Un profesor es igual a una clase si da la clase
+        /// </summary>
+        /// <param name="i"></param>
+        /// <param name="clase"></param>
+        /// <returns></returns>
+        public static bool operator ==(Profesor i, Universidad.EClases clase)
+        {
+            bool retorno = false;
+
+            foreach (Universidad.EClases c in i.clasesDelDia)
+            {
+                if (c == clase)
+                {
+                    retorno = true;
+                }
+            }
+
+            return retorno;
+        }
+        /// <summary>
+        /// Un profesor es distinto de una clase si no la da
+        /// </summary>
+        /// <param name="i"></param>
+        /// <param name="clase"></param>
+        /// <returns></returns>
+        public static bool operator !=(Profesor i, Universidad.EClases clase)
+        {
+            return !(i==clase);
+        }
+        /// <summary>
+        ///  se asignarán dos clases al azar al Profesor
+        /// </summary>
+        private void _randomClases()
+        {
+            this.clasesDelDia.Enqueue((Universidad.EClases)random.Next(0, 4));
+            this.clasesDelDia.Enqueue((Universidad.EClases)random.Next(0, 4));
+        }
+        /// <summary>
+        ///  retornará la cadena "CLASES DEL DÍA" junto al nombre de la clases que da
+        /// </summary>
+        /// <returns></returns>
+        protected override string ParticiparEnClase()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("\nCLASES DEL DIA: ");
+            foreach (Universidad.EClases c in this.clasesDelDia)
+            {
+                sb.AppendFormat("\n{0}", c);
+            }
+            return sb.ToString();
+        }
+        /// <summary>
+        /// Muestra los datos del profesor
+        /// </summary>
+        /// <returns></returns>
+        protected override string MostrarDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("{0}{1}.", base.MostrarDatos(), this.ParticiparEnClase());
+            return sb.ToString();
+        }
+        /// <summary>
+        /// Hace publicos los datos del profesor
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return this.MostrarDatos();
+        }
+
         #endregion
 
 
