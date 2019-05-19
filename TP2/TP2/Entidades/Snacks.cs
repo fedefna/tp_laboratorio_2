7c56cd3ce4f0c3732805103c61e01cd0442f35de
Alaniz Federico (Contractor) <ALANIZ@kapsch.net>
2019-05-19 18:16:40 -0300

    TP2

---
+++ TP2/TP2/Entidades/Snacks.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2018
+{
+    public class Snacks : Producto
+    {
+        /// <summary>
+        /// Constructor de snacks
+        /// </summary>
+        /// <param name="marca"></param>La marca del producto
+        /// <param name="patente"></param>El codigo de barras del producto
+        /// <param name="color"></param>El color del empaque
+        public Snacks(EMarca marca, string patente, ConsoleColor color)
+            : base(patente, marca, color)
+        {
+        }
+        /// <summary>
+        /// Los snacks tienen 104 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 104;
+            }
+        }
+
+        /// <summary>
+        /// Devuelve los datos del Producto.
+        /// </summary>
+        /// <returns></returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SNACKS");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("CALORIAS : {0}", this.CantidadCalorias);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
